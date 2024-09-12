e0d3f7117c4d4d93e340f4db237bd18afc036f6a
Cyanea <g3tsuzumi@gmail.com>
2024-09-12 21:59:54 +0900

    get_exit_status関数追加

--- srcs/main/minishell.c
+++ srcs/main/minishell.c
@@ -6,7 +6,7 @@
 /*   By: shonakam <shonakam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 15:57:54 by mosh              #+#    #+#             */
-/*   Updated: 2024/09/12 19:47:27 by shonakam         ###   ########.fr       */
+/*   Updated: 2024/09/12 21:53:21 by shonakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,8 @@ static void	minishell(t_minishell *mini)
 			continue ;
 		mini->cmd = build_commands(mini->token, count_tokens(mini->token));
 		ft_exec_v3(mini);
-		// ft_clean(mini, 0);
+		ft_clean(mini, 0);
+		get_exit_status(mini);
 		// return ;
 	}
 }
